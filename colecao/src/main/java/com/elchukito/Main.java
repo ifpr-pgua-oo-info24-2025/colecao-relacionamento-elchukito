29186f1d5a9d4f606aa0982de54271f442e869f6
Rafael da Silva Ferreira <146126647+elchukito@users.noreply.github.com>
2025-06-04 22:41:11 -0300

    Update Main.java

--- colecao/src/main/java/com/elchukito/Main.java
+++ colecao/src/main/java/com/elchukito/Main.java
@@ -18,8 +18,8 @@ public class Main {
         PhysicalBook physicalBook1 = new PhysicalBook("Python para iniciantes", 2016, authorList.get(0), 124);
         PhysicalBook physicalBook2 = new PhysicalBook("Kamasutra", 2001, authorList.get(2), 269);
     
-        Ebook ebook1 = new Ebook("13 Reasons why", 2010, authorList.get(1), 13.62);
-        Ebook ebook2 = new Ebook("How to produce in fl", 2021, authorList.get(3), 45.32);
+        Ebook ebook1 = new Ebook("13 Reasons why", 2010, authorList.get(1), 1.3);
+        Ebook ebook2 = new Ebook("How to produce in fl", 2021, authorList.get(3), 25.67);
 
         library.add(physicalBook1);
         library.add(physicalBook2);
@@ -27,5 +27,8 @@ public class Main {
         library.add(ebook2);
 
         library.get(0).viewDetails();
+        library.get(1).viewDetails();
+        library.get(2).viewDetails();
+        library.get(3).viewDetails();
     }
 }
